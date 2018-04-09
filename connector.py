0121df2ee8da52ad5788e9908622452a5eede922
Michelle Tham <mtham101@gmail.com>
2018-04-08 20:11:11 -0700

    added drop_table and query_sql methods

--- connector.py
+++ connector.py
@@ -1,24 +1,54 @@
+import time
 import psycopg2
+from psycopg2 import sql
 from private_settings import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
 
 
 def connect_to_db():
     try:
-        conn = psycopg2.connect(
-            "dbname={dbname} user={user} host={host} password={password}".format(dbname=DB_NAME, user=DB_USER, host=DB_HOST, password=DB_PASSWORD))
-        conn.autocommit = True
-    except:
-        print "I am unable to connect to the database"
-    cur = conn.cursor()
+        connection = psycopg2.connect(
+            "dbname={dbname} user={user} host={host} password={password}".format(
+                dbname=DB_NAME, user=DB_USER, host=DB_HOST, password=DB_PASSWORD
+            ))
+        connection.autocommit = True
+        cursor = connection.cursor()
+        # remember to close connection and cursor after executing a query
+        return connection, cursor
+    except Exception as e:
+        print('ERROR: Unable to connect to db --> ', e)
+
+
+def drop_table(cursor, table):
     try:
-        cur.execute("""CREATE TABLE test_customer_info (
-            ID INT PRIMARY KEY     NOT NULL,
-            NAME           TEXT    NOT NULL,
-            AGE            INT     NOT NULL,
-            ADDRESS        CHAR(50)
-        );""")
-    except:
-        print "I can't test the database!"
+        cursor.execute(
+            sql.SQL('DROP DATABASE IF EXISTS {};')
+            .format(sql.Identifier(table)))
+    except Exception as e:
+        print('ERROR: Unable to drop table --> ', e)
+
+
+def create_table(cursor, table):
+    try:
+        # CREATE TABLE IF NOT EXISTS
+    except Exception as e:
+        print('ERROR: Unable to create table --> ', e)
+
+
+def query_sql(cursor, sql):
+    queries = sql.split(';')
+
+    for query in queries:
+        if query:
+            print('QUERY ---> %s', query)
+            start = time.time()
+
+            try:
+                cursor.execute(query)
+            except Exception as e:
+                print('ERROR: Unable to execute query --> ', e)
+
+            print('Finished {query} in {time} seconds'.format(
+                time=time.time() - start, query=query))
 
 
 if __name__ == '__main__':
